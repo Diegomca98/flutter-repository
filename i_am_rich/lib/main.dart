d5f1439f78fd5d1e449379de0fbdec0e18522e18
Diegomca98 <diegomca98@gmail.com>
2023-06-27 19:27:52 -0600

    <Add>Partial add to the README and main.dart, video stopped at 9:08 mins

--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -2,9 +2,14 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
-      home: Center(
-        child: Text('Hello World'),
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Center(
+            child: Text("I Am Rich"),
+          ),
+          backgroundColor: Colors.blueGrey[900],
+        ),
       ),
     ),
   );
