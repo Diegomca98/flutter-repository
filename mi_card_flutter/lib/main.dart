08590b97c1850128a5c9236637b7619a80976197
Diegomca98 <diegomca98@gmail.com>
2023-07-06 15:20:43 -0600

    <Add>Code to main.dart from videos 39 to 41

--- mi_card_flutter/lib/main.dart
+++ mi_card_flutter/lib/main.dart
@@ -2,11 +2,30 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
-      ),
-    ),
+    MyCardApp(),
   );
+}
+
+class MyCardApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+        body: SafeArea(
+          child: Container(
+            height: 100.0,
+            width: 100.0,
+            margin: EdgeInsets.symmetric(
+              vertical: 50.0,
+              horizontal: 10.0,
+            ),
+            padding: EdgeInsets.all(20.0),
+            color: Colors.white,
+            child: Text('Hello World'),
+          ),
+        ),
+      ),
+    );
+  }
 }
