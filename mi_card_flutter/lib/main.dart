925db4ef56dcc776df8283e39fd8947cecec0ce5
Diegomca98 <diegomca98@gmail.com>
2023-08-02 14:40:55 -0600

    <Add>Mi Card App Completed

--- mi_card_flutter/lib/main.dart
+++ mi_card_flutter/lib/main.dart
@@ -14,6 +14,8 @@ class MyCardApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               CircleAvatar(
                 radius: 50.0,
@@ -26,6 +28,76 @@ class MyCardApp extends StatelessWidget {
                   fontSize: 40.0,
                   color: Colors.white,
                   fontWeight: FontWeight.bold,
+                  fontFamily: 'Handjet',
+                ),
+              ),
+              Text(
+                'Software Developer'.toUpperCase(),
+                style: TextStyle(
+                  fontSize: 15.0,
+                  color: Colors.white54,
+                  letterSpacing: 2.5,
+                  fontFamily: 'SourceSans',
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.teal.shade300,
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(
+                  vertical: 8.0,
+                  horizontal: 20.0,
+                ),
+                child: Padding(
+                  padding: EdgeInsets.symmetric(
+                    vertical: 5.0,
+                    horizontal: 10.0,
+                  ),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.call_outlined,
+                      color: Colors.teal.shade900,
+                    ),
+                    title: Text(
+                      "33 1273 3318",
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: 'SourceSans',
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(
+                  vertical: 8.0,
+                  horizontal: 20.0,
+                ),
+                child: Padding(
+                  padding: EdgeInsets.symmetric(
+                    vertical: 5.0,
+                    horizontal: 10.0,
+                  ),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.email_outlined,
+                      color: Colors.teal.shade900,
+                    ),
+                    title: Text(
+                      'diegomca98@gmail.com',
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: 'SourceSans',
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  ),
                 ),
               ),
             ],
