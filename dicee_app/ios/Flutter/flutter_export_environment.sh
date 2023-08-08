cb0ed6a0e225dc7639d66e5a96b2b42bd6fbbe30
Diegomca98 <diegomca98@gmail.com>
2023-08-08 00:20:16 -0600

    <Add>Created xylophone app, modified pubspec, created assets/ and README.MD mods

--- dicee_app/ios/Flutter/flutter_export_environment.sh
+++ dicee_app/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=F:\flutter\sdk\flutter_windows_3.3.10-stable\flutter"
-export "FLUTTER_APPLICATION_PATH=F:\Documentos\Cursos\angela-yu-courses\projects\flutter-repository\dicee_app"
+export "FLUTTER_ROOT=C:\Users\diego\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\diego\Documents\courses\dr-angela-yu\flutter-repository\dicee_app"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
