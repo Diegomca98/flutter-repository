32fe55768d62409a7592f2f8eb6e4cdca0f1c144
Diegomca98 <diegomca98@gmail.com>
2023-08-20 19:40:45 -0600

    <format>Python script output

--- get_flutter_dependencies.py
+++ get_flutter_dependencies.py
@@ -13,28 +13,17 @@ projects = os.listdir()
 for i in not_project_folders:
     if i in projects:
         projects.remove(i)
-
-flutter_ops = ['Create Project', 'Get Dependencies']
-
-print('Available Operations:')
-for i in range(len(flutter_ops)):
-    print(i+1, '->', flutter_ops[i])
-
-flutter_cmd = int(input('Choose the operation number: '))
-
-def create_flutter_project(prj_name, command='flutter create'):
-    creation = command + prj_name
-    os.system(creation)
     
 def get_dependencies(command='flutter pub get'):
     for i in projects:
-        print('Project,', i)
+        print('################')
+        print(i)
+        print('################')
         os.chdir(i)
         os.system(command)
         os.chdir('../')
+        print("<>=======================<>")
+        print('\n')
 
-if flutter_cmd == 1:
-    name = input("Type the project name: ")
-    create_flutter_project(name)
-elif flutter_cmd == 2:
-    get_dependencies()
+
+get_dependencies()
